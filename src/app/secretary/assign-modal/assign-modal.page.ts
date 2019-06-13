bd59796d2c585c4f408b67755222ba39cc336dc2
adm-lmalkhansa <adm-lmalkhansa@BE-09-048.darbeirut.com>
2019-06-13 16:28:52 +0300

    UUID, pass to modal

--- src/app/secretary/assign-modal/assign-modal.page.ts
+++ src/app/secretary/assign-modal/assign-modal.page.ts
@@ -1,24 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { AlertController, ModalController } from '@ionic/angular';
 import { ActionSheetController } from '@ionic/angular';
 import { SecretaryPage } from '../secretary.page'
+import { NavParams } from '@ionic/angular';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-assign-modal',
   templateUrl: './assign-modal.page.html',
   styleUrls: ['./assign-modal.page.scss'],
 })
-export class AssignModalPage implements OnInit {
 
+export class AssignModalPage implements OnInit {
+  public id: any;
   public array: any[];
   public priority:"";
-  constructor(private alertController: AlertController, private Actionsheet: ActionSheetController) { 
-    this.array=this.getArray();
+  public date:"";
+  public current_id:"";
+  constructor(private router: Router,private modalController:ModalController,private navParams:NavParams,private alertController: AlertController, private Actionsheet: ActionSheetController) { 
+    
+    
 
 
   }
   ngOnInit() {
-  
+   this.id=this.navParams.get('id');
+   console.log(this.id);
+   this.array=this.getArray();
   }
   getArray(): Object[] {
     var localStorageItem=JSON.parse(localStorage.getItem("ObjArray"));
@@ -37,7 +45,7 @@ export class AssignModalPage implements OnInit {
           handler:()=>
           {
             console.log("closed");
-            this.SaveRecord();
+            
 
             
           }
@@ -56,12 +64,27 @@ export class AssignModalPage implements OnInit {
     return await alert.present();
   }
 
-  SaveRecord()
+  async SaveRecord()
   {
-    if(this.priority==="")
-    console.log("empty")
-    else
-    console.log(this.priority);
+    var current_record=JSON.parse(this.id);
+    console.log(current_record);
+    var localStorageItem=JSON.parse(localStorage.getItem("ObjArray"));
+    console.log(localStorageItem);
+    
+    // var index=localStorageItem.indexOf(current_record);
+    // console.log(index);
+     current_record.priority=this.priority;
+     current_record.date=this.date;
+    // var newitem=temp;
+    // console.log(newitem);
+    // localStorageItem.splice(index,1,newitem);
+    // window.localStorage.setItem("ObjArray", JSON.stringify(localStorageItem));
+
+     this.modalController.dismiss(current_record);
   }
+  ngOnDestroy() {
+    // location.reload();
+    
+  } 
  
 }
--- src/app/secretary/secretary.page.ts
+++ src/app/secretary/secretary.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 
 import { ModalController } from '@ionic/angular';
 import { AssignModalPage } from '../secretary/assign-modal/assign-modal.page'
@@ -9,18 +9,19 @@ import { AssignModalPage } from '../secretary/assign-modal/assign-modal.page'
   styleUrls: ['./secretary.page.scss'],
 })
 export class SecretaryPage implements OnInit {
-
+  @Input() id: any;
   public array: any[];
   public item1:any;
   public static mc:any;
   constructor(public modalController: ModalController) { 
-    this.array=this.getArray();
-    
+  this.array=this.getArray();
+  const uuidv1 = require('uuid/v1');
+
     window.localStorage.setItem("ObjArray", JSON.stringify([
-      {"id":1,"type":"txt1","priority":"low","date":"1/1/2009","name":"name1"},
-      {"id":2,"type":"txt1","priority":"low","date":"1/1/2009","name":"name2"},
-      {"id":3,"type":"txt1","priority":"low","date":"1/1/2009","name":"name3"}, 
-      {"id":4,"type":"txt1","priority":"low","date":"1/1/2009","name":"name4"}
+      {"id":uuidv1(),"type":"txt1","priority":"low","date":"1/1/2009","name":"name1"},
+      {"id":uuidv1(),"type":"txt1","priority":"low","date":"1/1/2009","name":"name2"},
+      {"id":uuidv1(),"type":"txt1","priority":"low","date":"1/1/2009","name":"name3"}, 
+      {"id":uuidv1(),"type":"txt1","priority":"low","date":"1/1/2009","name":"name4"}
     ]))
 
   }
@@ -29,12 +30,23 @@ export class SecretaryPage implements OnInit {
     SecretaryPage.mc=this.modalController;
   
   }
-async assignModal()
+async assignModal(event)
 {
-  const modal= await this.modalController.create({
-  component: AssignModalPage
+    var target = event.target || event.srcElement || event.currentTarget;
+    var idAttr = target.attributes.id;
+    var value = idAttr.nodeValue;
+    var record=JSON.stringify(JSON.parse(localStorage.getItem("ObjArray")).find(x => x.id === JSON.parse(value)));
+    var localStorageItem=JSON.parse(localStorage.getItem("ObjArray"));
+    const modal= await this.modalController.create({
+  component: AssignModalPage,
+  componentProps: {id: record}
   }
   );
+  modal.onDidDismiss()
+  .then((data) => {
+        const current_record = data['data']; 
+        console.log(current_record);
+    });
   await modal.present();
 }
 
