f583e59e2b3066e780150c55399142b504be8091
Karim Naous <kynaous@darbeirut.com>
2019-06-14 12:10:12 +0300

    "

--- src/app/admin/admin.page.ts
+++ src/app/admin/admin.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import  { ModalController } from '@ionic/angular';
 import { ModalAdminPage } from '../modal-admin/modal-admin.page'
+import { INTERNAL_BROWSER_PLATFORM_PROVIDERS } from '@angular/platform-browser/src/browser';
 
 @Component({
   selector: 'app-home',
@@ -13,27 +14,26 @@ export class AdminPage implements OnInit {
   dataReturned:any;
 
   ngOnInit() {
+    localStorage.clear();
+    let employee =  [
+      {Name:"Jason", Gender:"Male", Birthdate:"Jun-30-1999", Role:"Administrator" , Department:"IT", MaritalStatus:"Married", Notes:"None"},
+      {Name:"Alex", Gender:"Male", Birthdate:"Jan-02-1999", Role:"Secretary" , Department:"IT", MaritalStatus:"Single", Notes:"None"},
+      {Name:"Karim", Gender:"Male", Birthdate:"Dec-16-1999", Role:"Employee" , Department:"Accounting", MaritalStatus:"Single", Notes:"None"},
+      {Name:"Leen", Gender:"Female", Birthdate:"Feb-07-1999", Role:"Officer" , Department:"IT", MaritalStatus:"Single", Notes:"None"}
+    ]; 
+    localStorage.setItem("employee", JSON.stringify(employee));
   }
-
-
+    
   constructor(
     public modalController: ModalController
-  ) {
+  ) {}
 
-    let employee1 = {Name:"jason", Birthdate:"Jun-01-1999", Role:"administrator" , Department:"IT", MaritalStatus:"Single", Notes:"None"};
-    let employee2 = {Name:"jason", Birthdate:"Jun-01-1999", Role:"administrator" , Department:"IT", MaritalStatus:"Single", Notes:"None"};
-    let employee3 = {Name:"jason", Birthdate:"Jun-01-1999", Role:"administrator" , Department:"IT", MaritalStatus:"Single", Notes:"None"};
-    // localStorage.setItem("employee1", JSON.stringify(employee1));
-    localStorage.setItem("employee2", JSON.stringify(employee2));
-    localStorage.setItem("employee3", JSON.stringify(employee3));
-  }
-
-
-    
-  async openModal() {
+  async openModal(id : number) {
     const modal = await this.modalController.create({
       component: ModalAdminPage,
       componentProps: {
+        "arrayEmps" : this.getEmployees(),
+        "ID" : id, 
         "paramID": 123,
         "paramTitle": "Test Title"
       }
@@ -42,20 +42,29 @@ export class AdminPage implements OnInit {
     modal.onDidDismiss().then((dataReturned) => {
       if (dataReturned !== null) {
         this.dataReturned = dataReturned.data;
-        //alert('Modal Sent Data :'+ dataReturned);
       }
+      console.log(this.dataReturned);
+      this.saveEmployees(this.dataReturned);
     });
  
     return await modal.present();
   }
 
-  HEROES = [
-    {id: 1, name:'Superman'},
-    {id: 2, name:'Batman'},
-    {id: 5, name:'BatGirl'},
-    {id: 3, name:'Robin'},
-    {id: 4, name:'Flash'}
-  ];
+  public saveEmployees(emps:object) {
+      localStorage.setItem("employee", JSON.stringify(emps))
+  }
 
+  public getEmployees() : Object {
+      if (localStorage.length > 0) {
+        let myJSON = localStorage.getItem('employee');
+        let myObj = JSON.parse(myJSON);
+        return myObj;
+      }
+      else {
+        return [];
+      }
+  }
+
+  employees = this.getEmployees();
 
 }
--- src/app/modal-admin/modal-admin.page.ts
+++ src/app/modal-admin/modal-admin.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController, NavParams } from '@ionic/angular';
+import { empty } from 'rxjs';
 
 @Component({
   selector: 'app-modal-admin',
@@ -10,6 +11,20 @@ export class ModalAdminPage implements OnInit {
 
   modalTitle:string;
   modelId:number;
+  arrayEmps: [any];
+  ID: number; 
+
+  public name : string; 
+  public gender : string; 
+  public date : string;
+  public department : string; 
+  // public departments : [any];
+  // checkbox returns true up 
+  public maritalStatus : boolean; 
+  // radio return undefined if unselected
+  public role : string;
+  public notes : string;
+  
 
   constructor(
     private modalController: ModalController,
@@ -20,10 +35,53 @@ export class ModalAdminPage implements OnInit {
     console.table(this.navParams);
     this.modelId = this.navParams.data.paramID;
     this.modalTitle = this.navParams.data.paramTitle;
+    this.ID = this.navParams.data.ID;
+    this.arrayEmps = this.navParams.data.arrayEmps;
+    this.readEmployee(this.arrayEmps[this.ID]);
+    // console.log(this.arrayEmps[this.ID].Name)
+    console.log(this.departments[0]["isChecked"]);
+    // this.departments[0]["isChecked"] = 
+    if (this.department == "Accounting") { 
+        this.departments[0]["isChecked"] = true;
+    }
+     
   }
 
-  async closeModal() {
-    const onClosedData: string = "Wrapped Up!";
-    await this.modalController.dismiss(onClosedData);
+  public readEmployee(emp : object) {
+      this.name = emp["Name"];
+      this.gender = emp["Gender"];
+      this.date = emp["Birthdate"];
+      this.department = emp["Department"];
+      if (emp["MaritalStatus"] == "Married") { this.maritalStatus = true; } 
+      else { this.maritalStatus = false; }
+      this.role = emp["Role"];
+      this.notes = emp["Notes"];
+  } 
+
+  async closeModal(save:boolean) {
+    if (save == true) {
+        let married = "Single";
+        if (this.maritalStatus) { married = "Married"; }
+        let emp = {Name : this.name, Gender:this.gender, Birthdate : this.date, Role : this.role , Department : this.department, MaritalStatus : married, Notes:this.notes};
+        // console.log(emp);
+        this.arrayEmps[this.ID] = emp;
+        // console.log(this.arrayEmps);
+        const onClosedData: [any] = this.arrayEmps;
+        console.log(onClosedData);
+        await this.modalController.dismiss(onClosedData);
+    }
+    else {
+        const onClosedData: [any] = this.arrayEmps;
+        await this.modalController.dismiss(onClosedData);
+    }
   }
+
+public departments = [
+    {val: "Accounting", isChecked: false},
+    {val: "Engineering", isChecked: false},
+    {val: "Human Resources", isChecked: false},
+    {val: "Information Technology", isChecked: false},
+];
+
+
 }
